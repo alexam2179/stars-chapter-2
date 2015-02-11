1006dccf477829e90081e07fc50005f06a3e4376
23326809 <23326809@CST_N417_05.cst.citytech.cuny.edu>
2015-02-11 17:10:29 -0500

    stars

--- src/chapter2/CHAPTER2.java
+++ src/chapter2/CHAPTER2.java
@@ -16,7 +16,16 @@ public class CHAPTER2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("     *");
+       System.out.println("    ***");
+       System.out.println("   ******");
+       System.out.println("  ********");
+       System.out.println("   ******");
+       System.out.println("    ***");
+       System.out.println("     *");
+       
+       System.out.println("   *\n  ***\n *****\n*******\n *****\n  ***\n   *");
     }
+    
     
 }
